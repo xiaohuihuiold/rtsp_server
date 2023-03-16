856d4b12a13486771f956502a809221e0d7ad6a1
xiaohuihuiold <xiaohuihuiold@gmail.com>
2023-03-16 10:06:57 +0800

    feat(logger): 优化日志输出

--- lib/src/logger.dart
+++ lib/src/logger.dart
@@ -1,12 +1,19 @@
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:date_format/date_format.dart';
 import 'package:logger/logger.dart';
 
 final logger = LoggerWrapper();
 
 /// 自定义日志
 class LoggerWrapper {
-  final logger = Logger(printer: SimplePrinter());
+  final logger = Logger(
+    printer: HybridPrinter(
+      MySimplePrinter(),
+      error: PrettyPrinter(printTime: true),
+    ),
+  );
 
   void v(
     dynamic message, {
@@ -113,5 +120,37 @@ class LoggerWrapper {
 
   String? _getAddress(Socket? socket) {
     return socket?.remoteAddress.address;
+  }
+}
+
+/// 自定义简单日志
+class MySimplePrinter extends SimplePrinter {
+  static final grey = AnsiColor.fg(8);
+  static final timeFormat = [HH, ':', nn, ':', ss];
+
+  MySimplePrinter() : super(printTime: true, colors: true);
+
+  @override
+  List<String> log(LogEvent event) {
+    var messageStr = _stringifyMessage(event.message);
+    var errorStr = event.error != null ? '  ERROR: ${event.error}' : '';
+    var timeStr = printTime ? grey(formatDate(event.time, timeFormat)) : '';
+    return ['${_labelFor(event.level)} $timeStr $messageStr$errorStr'];
+  }
+
+  String _labelFor(Level level) {
+    var prefix = SimplePrinter.levelPrefixes[level]!;
+    var color = SimplePrinter.levelColors[level]!;
+    return colors ? color(prefix) : prefix;
+  }
+
+  String _stringifyMessage(dynamic message) {
+    final finalMessage = message is Function ? message() : message;
+    if (finalMessage is Map || finalMessage is Iterable) {
+      var encoder = const JsonEncoder.withIndent(null);
+      return encoder.convert(finalMessage);
+    } else {
+      return finalMessage.toString();
+    }
   }
 }
